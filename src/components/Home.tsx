027e17f084ce870ab7006a29a05c26f819386ee8
Andreea Racovita <andreearacovitagit@gmail.com>
2025-10-14 13:58:20 +0300

    Boilerplate code

---
+++ src/components/Home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+	
+	return (
+		<h1 className="archivo-black-regular">Landing page</h1>
+	);
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+	
+	return (
+		<header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between my-3">
+			<div className="col-md-3 mb-2 mb-md-0">
+				<a href="#" className="d-inline-flex link-body-emphasis text-decoration-none align-items-center">
+					<img src="logo-yellow.png" height="40" />
+					<span className="archivo-black-regular logo-title ms-1">emento</span>
+				</a>
+			</div>
+
+			<ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+				<li>
+					<Link to="#" className="nav-link px-2 text-uppercase">Complexity</Link>
+				</li>
+				<li>
+					<Link to="#" className="nav-link px-2 text-uppercase">JS Library</Link>
+				</li>
+				<li>
+					<Link to="#" className="nav-link px-2 text-uppercase">JS Definitive Edition</Link>
+				</li>
+			</ul>
+		</header>
+	);
+}
