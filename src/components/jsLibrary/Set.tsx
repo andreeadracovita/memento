fb0ef3341fe03826f3f73d070d5da5df6fb9f48a
Andreea Racovita <andreearacovitagit@gmail.com>
2026-01-15 14:10:19 +0200

    Add content: Set

--- src/components/jsLibrary/Set.tsx
+++ src/components/jsLibrary/Set.tsx
@@ -4,7 +4,37 @@ import CardHeader from "components/CardHeader";
 
 export default function Set() {
 	const [toggle, setToggle] = useState(false);
-	const code = `TODO`;
+	const code = `// Create a Set
+let s = new Set();         // empty set
+let t = new Set([1, s]);   // new set with 2 members
+let u = new Set(s);        // new set that copies the elements of s
+let v = new Set("Apples"); // 5 elements: "A", "p", "l", "e", "s"
+v.size                     // 5
+
+let mySet = new Set();
+s.size // 0
+s.add(1);
+s.add(true);
+s.add([1,2,3]);
+s.delete(1);
+s.delete("test"); // false
+s.clear();
+
+let primes = new Set([2,3,5,7]);
+primes.has(2) // true
+primes.has(8) // false
+
+let sum = 0;
+for(let p of primes) {
+    sum += p;
+}
+
+[...primes]
+Math.max(...primes)
+
+let product = 1;
+primes.forEach(n => { product *= n; });
+`;
 
 	return (
 		<div className="grey-card">
@@ -17,7 +47,15 @@ export default function Set() {
 			{
 				toggle &&
 				<>
-					<p>Set = a collection of values, not ordered/indexed, does not allow duplicates</p>
+					<ul>
+						<li>Set = a collection of values, unordered/indexed, does not allow duplicates.</li>
+						<li>★ Sets are optimized for membership testing, no matter how many members it has. Using an array as a set will perform much slower than a real set.</li>
+						<li><strong>add()</strong> method takes a single argument. If passing an array, the array itself is added to the set, not each individual element. It returns the set it is invoked on.</li>
+						<li><strong>delete()</strong> deletes a single set element. Returns a boolean (true if the value was in the set before deletion, false otherwise).</li>
+						<li>Performs string equality check ===. mySet.add([1,2]); mySet.delete([1,2]) returns false.</li>
+						<li>The Set class is iterable, can use for/of loop to enumerate all the elements in a set. Can also convert them to arrays and argument list using ... spread operator.</li>
+						<li>A JS set remembers the order in which the elements were inserted in, uses this order when iterating.</li>
+					</ul>
 					<pre>{code}</pre>
 				</>
 			}
