e4c060607924823f34bf830503005ac17e3f87a5
Andreea Racovita <andreearacovitagit@gmail.com>
2026-03-03 18:08:07 +0200

    Add examples of interview questions

--- src/components/Concepts.tsx
+++ src/components/Concepts.tsx
@@ -3,11 +3,42 @@ import Memory from "components/concepts/Memory";
 import ObjectOriented from "components/concepts/ObjectOriented";
 import SystemDesignScalability from "components/concepts/SystemDesignScalability";
 import Testing from "components/concepts/Testing";
+import CardHeader from "components/CardHeader";
 
 export default function Concepts() {
 	return (
 		<>
 			<h1 className="archivo-black-regular">Concepts</h1>
+			<div className="grey-card">
+				<CardHeader
+					title={"Generic Computer Science Questions"}
+					toggle={true}
+				/>
+				<ul>
+					<li>Difference between static and dynamic libraries? Who loads the dynamic libraries?</li>
+					<li>What is virtual memory and what is it used for?</li>
+					<li>Does malloc allocate virtual memory or physical memory?</li>
+					<li>What memory addresses is the CPU using when executing a program?</li>
+					<li>Who translates virtual memory into physical memory? (MMU, TLB)</li>
+					<li>What is a segmentation fault?</li>
+					<li>What are memory pages?</li>
+					<li>Process in-memory layout/structure (Heap, Stack, Text, Data)</li>
+					<li>What is stack overflow?</li>
+					<li>What is buffer overflow?</li>
+					<li>Application vs Process vs Thread</li>
+					<li>Do threads have private memory? (stack, TLS)</li>
+					<li>How is communication between threads achieved?</li>
+					<li>What is a race condition?</li>
+					<li>Describe a deadlock scenario. What is the difference between a deadlock and a livelock?</li>
+					<li>Mutex vs Semaphore</li>
+					<li>Mutex vs Spinlock</li>
+					<li>What other synchronisation primitives are there? How do they work?</li>
+					<li>Inter Process Communication examples</li>
+					<li>Unit testing vs integration testing, any other examples of testing</li>
+					<li>Examples of design patters and their description</li>
+				</ul>
+			</div>
+
 			<BitManipulation />
 			<Memory />
 			<ObjectOriented />
--- src/components/DataStructures.tsx
+++ src/components/DataStructures.tsx
@@ -5,11 +5,33 @@ import LinkedLists from "components/dataStructures/LinkedLists";
 import Queues from "components/dataStructures/Queues";
 import Stacks from "components/dataStructures/Stacks";
 import Trees from "components/dataStructures/Trees";
+import CardHeader from "components/CardHeader";
 
 export default function DataStructures() {
 	return (
 		<>
 			<h1 className="archivo-black-regular">Data Structures</h1>
+
+			<div className="grey-card">
+				<CardHeader
+					title={"Data Structures and Algorithms Questions"}
+					toggle={true}
+				/>
+				<ul>
+					<li>Stack vs Queue, complexity for common operations</li>
+					<li>Linked list vs vector</li>
+					<li>Priority Queue and how to implement it. Example from STL</li>
+					<li>What is a deque? How is std::deque implemented?</li>
+					<li>Hashtable vs Binary (Search) Tree? (Bonus: Heap, Treap, Trie)</li>
+					<li>Map vs unordered_map</li>
+					<li>Map implementation (unordered - hash map, ordered - balanced search tree, good hashing function, collision resolution, complexity time/space)</li>
+					<li>Tree types</li>
+					<li>Hash Table vs ...</li>
+					<li>DFS vs BFS</li>
+					<li>Examples of Binary Tree traversals</li>
+				</ul>
+			</div>
+
 			<ArraysStrings />
 			<LinkedLists />
 			<Stacks />
